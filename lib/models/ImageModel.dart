631b3c8b8d3f17ebfdba5e97b321b3344f6e19e3
Reingalrt <adorufus@gmail.com>
2020-02-24 20:37:13 +0700

    commit

---
+++ lib/models/ImageModel.dart
@@ -0,0 +1,10 @@
+class ImageModel {
+  final String id;
+  final String title;
+  final String desc;
+  final String imageUrl;
+
+  ImageModel({this.id, this.title, this.desc, this.imageUrl});
+
+  
+}
